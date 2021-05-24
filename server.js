50111d39451d8a5d42253117869cc11d93769c03
Alex Curran <alexcurran.va@gmail.com>
2021-05-24 16:56:11 -0500

    Added server listening code

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 "use strict";
+
+require("dotenv").config();
+
+// Imports
+const express = require("express");
+const PORT = process.env.PORT || 5000;
+const morgan = require("morgan");
+
+const app = express();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(morgan("dev"));
+}
+
+// Middleware
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.listen(PORT, () => console.log(`Server listening on port, ${PORT}.`));
