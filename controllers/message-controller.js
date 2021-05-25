d335f2a9a30727521766c993cdbf5235cc98ba86
Alex Curran <alexcurran.va@gmail.com>
2021-05-25 10:01:07 -0500

    Add controller and route files for messages, also updated server.js to enable router

---
+++ controllers/message-controller.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// GET controllers
+async function getIndex(req, res, next) {
+    try {
+        
+    } catch (err) {
+        next(err)
+    }
+}
+
+// POST controllers
+
+
+module.exports = {
+    getIndex,
+};
--- server.js
+++ server.js
@@ -6,10 +6,13 @@ require("dotenv").config();
 const express = require("express");
 const PORT = process.env.PORT || 5000;
 const morgan = require("morgan");
+// Routes
+const messageRouter = require("./routes/message-router");
 
 const app = express();
 
-if (process.env.NODE_ENV === "production") {
+// Dev logging
+if (process.env.NODE_ENV !== "production") {
     app.use(morgan("dev"));
 }
 
@@ -18,5 +21,15 @@ app.set("view engine", "ejs");
 // Middleware
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+// Enable routers
+app.use("/", messageRouter);
+
+// General server error handling
+app.use((err, req, res, next) => {
+    console.error(err);
+
+    // TODO: Render error page
+    // return res.render()
+});
 
 app.listen(PORT, () => console.log(`Server listening on port, ${PORT}.`));
