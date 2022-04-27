b90b6c8d533d500273bdfcfb4099783c525e1497
Alex Curran <alexcurran.va@gmail.com>
2022-04-27 14:53:32 -0500

    Update db schema to alter content column data type to TEXT (postgres specific) for indeterminate char lengths

--- db/db_schema.sql
+++ db/db_schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE user_message(
     message_id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
-    content VARCHAR NOT NULL,
+    content TEXT NOT NULL,
     topic VARCHAR(30) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE
 );
